67342fcc0dc1bfeb04ddabf2b3a0d2e05178ec63
Sanal Mishra <16-sanalmishra>
2022-05-23 12:37:01 +0530

    ngSwitch

--- my-app1/src/app/numbers/numbers.component.ts
+++ my-app1/src/app/numbers/numbers.component.ts
@@ -8,6 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class NumbersComponent implements OnInit {
   numbers=[1,2,3,4,5,5,6];
   status=true;
+  boxNumber=11;
   constructor() { }
 
   ngOnInit(): void {
