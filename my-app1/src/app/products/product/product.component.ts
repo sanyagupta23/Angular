870ef42bf59fa8f12c094c9207d23efb800f3746
Sanal Mishra <16-sanalmishra>
2022-05-23 22:17:38 +0530

    Added lifecycle Methods

--- my-app1/src/app/products/product/product.component.ts
+++ my-app1/src/app/products/product/product.component.ts
@@ -1,20 +1,65 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-product',
   templateUrl: './product.component.html',
-  styleUrls: ['./product.component.css']
+  styleUrls: ['./product.component.css'],
 })
-export class ProductComponent implements OnInit {
-@Input()
-  productElement :{
-    name:string;
-    stock:number;
-    availability:string;
+// implements
+//   OnInit,
+//   OnChanges,
+//   DoCheck,
+//   AfterContentInit,
+//   AfterContentChecked,
+//   AfterViewInit,
+//   AfterViewChecked,
+//   OnDestroy
+export class ProductComponent {
+  @Input()
+  productElement: {
+    name: string;
+    stock: number;
+    availability: string;
   };
-  constructor() { }
 
-  ngOnInit(): void {
-  }
+  // constructor() {
+  //   console.log('Product constructor called..');
+  // }
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   console.log('Product ngOnChanges called..');
+  // }
+  // ngDoCheck(): void {
+  //   console.log('Product ngDoCheck called..');
+  // }
+  // ngAfterContentInit(): void {
+  //   console.log('Product ngAfterContentInit called..');
+  // }
+  // ngAfterContentChecked(): void {
+  //   console.log('Product ngAfterContentChecked called..');
+  // }
+  // ngAfterViewInit(): void {
+  //   console.log('Product ngAfterViewInit called..');
+  // }
+  // ngAfterViewChecked(): void {
+  //   console.log('Product ngAfterViewChecked called..');
+  // }
+  // ngOnDestroy(): void {
+  //   console.log('Product ngOnDestroy called..');
+  // }
 
+  // ngOnInit(): void {
+  //   console.log('Product ngOnInit called..');
+  // }
 }
--- my-app1/src/app/products/products.component.ts
+++ my-app1/src/app/products/products.component.ts
@@ -1,27 +1,72 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css'],
 })
-export class ProductsComponent implements OnInit {
+// implements
+//   OnInit,
+//   OnChanges,
+//   DoCheck,
+//   AfterContentInit,
+//   AfterContentChecked,
+//   AfterViewInit,
+//   AfterViewChecked,
+//   OnDestroy
+export class ProductsComponent {
   products = [
     {
       name: 'Laptop',
       stock: 200,
       availability: 'In Stock',
     },
-    {
-      name: 'Phone',
-      stock: 100,
-      availability: 'In Stock',
-    },
+    // {
+    //   name: 'Phone',
+    //   stock: 100,
+    //   availability: 'In Stock',
+    // },
   ];
 
-  constructor() {}
+  // constructor() {
+  //   console.log('Products constructor called..');
+  // }
+  // ngOnChanges(changes: SimpleChanges): void {
+  //   console.log('Products ngOnChanges called..');
+  // }
+  // ngDoCheck(): void {
+  //   console.log('Products ngDoCheck called..');
+  // }
+  // ngAfterContentInit(): void {
+  //   console.log('Products ngAfterContentInit called..');
+  // }
+  // ngAfterContentChecked(): void {
+  //   console.log('Products ngAfterContentChecked called..');
+  // }
+  // ngAfterViewInit(): void {
+  //   console.log('Products ngAfterViewInit called..');
+  // }
+  // ngAfterViewChecked(): void {
+  //   console.log('Products ngAfterViewChecked called..');
+  // }
+  // ngOnDestroy(): void {
+  //   console.log('Products ngOnDestroy called..');
+  // }
 
-  ngOnInit(): void {}
+  // ngOnInit(): void {
+  //   console.log('Products ngOnInit called..');
+  // }
 
   onProductAdded(data) {
     this.products.push({
