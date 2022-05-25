f15fb1e114d48ea1b6a723c892b8b9a4e8c3d67b
Sanal Mishra <16-sanalmishra>
2022-05-25 10:59:22 +0530

    Pipes

--- my-app1/src/app/observables/observables.component.ts
+++ my-app1/src/app/observables/observables.component.ts
@@ -1,41 +1,61 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-
-import { interval,Observable,Subscription } from 'rxjs';
+import { interval, Observable, Subscription } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-observables',
   templateUrl: './observables.component.html',
-  styleUrls: ['./observables.component.css']
+  styleUrls: ['./observables.component.css'],
 })
-export class ObservablesComponent implements OnInit , OnDestroy{
-private intervalSubs:Subscription;
-private customIntervalSubscription:Subscription;
-  constructor() { }
-  
-  
+export class ObservablesComponent implements OnInit, OnDestroy {
+  private intervalObsSubscription: Subscription;
+  private customIntervalSubscription: Subscription;
+
+  constructor() {}
 
   ngOnInit(): void {
-    this.intervalSubs = interval(1000).subscribe((count) => {
-      console.log(count);
-    });
+    // this.intervalObsSubscription = interval(1000).subscribe((count) => {
+    //   console.log(count);
+    // });
 
     const customIntervalObservable = Observable.create((observer) => {
       let count = 0;
       setInterval(() => {
-        observer.next('X' + count);
+        observer.next(count);
         count++;
-      }, 1500);
+        if (count === 4) {
+          observer.complete();
+        }
+        if (count > 4) {
+          observer.error(new Error('count is greater than 2'));
+        }
+      }, 2000);
     });
 
-    this.customIntervalSubscription = customIntervalObservable.subscribe(
-      (count) => {
-        console.log(count);
-      }
-    );
+    this.customIntervalSubscription = customIntervalObservable
+      .pipe(
+        filter((data: number) => {
+          return data % 2 === 0;
+        }),
+        map((data: number) => {
+          return 'Round: ' + data;
+        })
+      )
+      .subscribe(
+        (count) => {
+          console.log(count);
+        },
+        (error) => {
+          console.log('error occurred ->' + error);
+        },
+        () => {
+          console.log('Completed');
+        }
+      );
+  }
 
-}
-ngOnDestroy(): void {
-  this.intervalSubs.unsubscribe();
-  this.customIntervalSubscription.unsubscribe();
-}
+  ngOnDestroy(): void {
+    // this.intervalObsSubscription.unsubscribe();
+    this.customIntervalSubscription.unsubscribe();
+  }
 }
